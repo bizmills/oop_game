295b6a85cb8796928f05f45567ae3458419d9301
Hackbright Student <info@hackbrightacademy.com>
2014-06-11 16:54:48 -0700

    weapon, hp counter and del dead works

--- game.py
+++ game.py
@@ -3,6 +3,8 @@ import pyglet
 from pyglet.window import key
 from core import GameElement
 import sys
+from random import random
+import time
 
 #### DO NOT TOUCH ####
 GAME_BOARD = None
@@ -11,20 +13,32 @@ KEYBOARD = None
 PLAYER = None
 ######################
 
-GAME_WIDTH = 5
-GAME_HEIGHT = 5
+GAME_WIDTH = 10
+GAME_HEIGHT = 10
 
 #### Put class definitions here ####
-class Gem(GameElement):
-    IMAGE = "BlueGem"
+class Shiv(GameElement):
+    IMAGE = "Shiv"
     SOLID = False
 
+    def __repr__(self):
+        return "shiv"
+
+    def interact(self, player):
+        player.inventory["shiv"] = player.inventory.get("shiv", 0) + 1
+        GAME_BOARD.draw_msg("You just acquired a %s! You have %d items!"%("shiv", len(player.inventory)))
+        print player.inventory
+class Grave(GameElement):
+    IMAGE = "Heart" # To be replaced by grave stone image
+    SOLID = False
 class Rock(GameElement):
     IMAGE = "Rock"
     SOLID = True
 
 class Character(GameElement):
     IMAGE = "Horns"
+    HP = 100
+    Strength = int(random() * 100)
 
     def next_pos(self, direction):
         if direction == "up":
@@ -36,6 +50,43 @@ class Character(GameElement):
         elif direction == "right":
             return (self.x+1, self.y)
         return None
+
+    def __init__(self):
+        GameElement.__init__(self)
+        self.inventory = {}
+
+class Monster(GameElement):
+    IMAGE = "Princess"
+    SOLID = True
+    HP = 100
+    Strength = int(random() * 50)
+
+    def interact(self, player):
+        if "shiv" in player.inventory:
+            damage_to_monster = player.Strength
+            damage_to_player = self.Strength
+            self.HP -= damage_to_monster
+            player.HP -= damage_to_player
+            GAME_BOARD.draw_msg("You did %d damage and the monster did %d damage! You have %d health remaining" % (damage_to_monster, damage_to_player, player.HP))
+
+            if player.HP <= 0:
+                GAME_BOARD.draw_msg("You died.")
+            if self.HP <= 0:
+                GAME_BOARD.draw_msg("You killed it! Here's some $$$...just kidding")
+                GAME_BOARD.del_el(4,4)
+                grave = Grave()
+                GAME_BOARD.register(grave)
+                GAME_BOARD.set_el(4,4, grave)
+        else:
+            GAME_BOARD.draw_msg("The monster laughs at your lack of a weapon.")
+
+#This may work eventually to delay an image
+            # time.sleep(10)
+            # GAME_BOARD.del_el(4,4)
+            # rock = Rock()
+            # GAME_BOARD.register(rock)
+            # GAME_BOARD.set_el(4,4, rock)
+
 ####   End class definitions    ####
 
 def keyboard_handler():
@@ -58,6 +109,9 @@ def keyboard_handler():
 
         existing_el = GAME_BOARD.get_el(next_x, next_y)
 
+        if existing_el:
+            existing_el.interact(PLAYER)
+
         if existing_el is None or not existing_el.SOLID:
             GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
             GAME_BOARD.set_el(next_x, next_y, PLAYER)
@@ -66,10 +120,10 @@ def initialize():
     """Put game initialization code here"""
     GAME_BOARD.draw_msg("Linna and Biz made this:)")
 
-    gem = Gem()
-    GAME_BOARD.register(gem)
-    GAME_BOARD.set_el(3,1, gem)
-    
+    shiv = Shiv()
+    GAME_BOARD.register(shiv)
+    GAME_BOARD.set_el(3,1, shiv)
+
     global PLAYER
     PLAYER = Character()
     GAME_BOARD.register(PLAYER)
@@ -96,22 +150,6 @@ def initialize():
     for rock in rocks:
         print rock
 
-    # rock1 = Rock()
-    # GAME_BOARD.register(rock1)
-    # GAME_BOARD.set_el(1, 1, rock1)
-
-    # # Initialize and register rock 2
-    # rock2 = Rock()
-    # GAME_BOARD.register(rock2)
-    # GAME_BOARD.set_el(2, 2, rock2)
-
-    # rock3 = Rock()
-    # GAME_BOARD.register(rock3)
-    # GAME_BOARD.set_el(2,2, rock3)
-
-    # print "The first rock is at", (rock1.x, rock1.y)
-    # print "The second rock is at", (rock2.x, rock2.y)
-    # print "The third rock is at", (rock3.x, rock3.y)
-    # print "Rock 1 image", rock1.IMAGE
-    # print "Rock 2 image", rock2.IMAGE
-    # print "Rock 3 image", rock3.IMAGE
+    princess = Monster()
+    GAME_BOARD.register(princess)
+    GAME_BOARD.set_el(4,4, princess)
